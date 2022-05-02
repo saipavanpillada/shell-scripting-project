5e188f17dd42029e341c092322dece7247e4aae8
saipavanpillada <varains2116@gmail.com>
2022-05-02 17:10:51 +0530

    update1

---
+++ arg.sh
@@ -0,0 +1,10 @@
+echo "Arguments total : $# "
+echo "script name : $0 "
+echo "script 1st argument : $1 "
+echo "script 2nd argument : $2 "
+echo "script 3rd argument : $3 "
+echo "list of all arguments : $* "
+echo "list of all arguments : $@ "
+echo "pid of shell : $$ "
+
+
